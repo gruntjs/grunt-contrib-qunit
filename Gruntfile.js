733348d49531dffed0e4ed77e1529a2ab9310ca9
Tyler Kellen <tyler@sleekcode.net>
2012-10-05 13:52:52 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/*
+ * grunt-contrib-qunit
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/**/*.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // By default, lint task.
+  grunt.registerTask('default', 'jshint');
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# grunt-contrib-qunit [![Build Status](https://secure.travis-ci.org/gruntjs/grunt-contrib-qunit.png?branch=master)](http://travis-ci.org/gruntjs/grunt-contrib-qunit) (unreleased, for grunt v0.4.0a)
+
+> Run QUnit unit tests in a headless PhantomJS instance.
