4d8599c1b2eb29a9873749b24dbb39e37bb08529
Sam Kirkpatrick <=>
2016-04-12 09:18:08 +0100

    Removed 'grunt.warn' output on error - onus now on end user binding to event

--- tasks/qunit.js
+++ tasks/qunit.js
@@ -150,10 +150,6 @@ module.exports = function(grunt) {
 
   phantomjs.on('error.onError', function (msg, stackTrace) {
     grunt.event.emit('qunit.error.onError', msg, stackTrace);
-
-    if (options && !options.summaryOnly) {
-      grunt.log.warn('PhantomJS error:\n', msg, '\n', stackTrace);
-    }
   });
 
   grunt.registerMultiTask('qunit', 'Run QUnit unit tests in a headless PhantomJS instance.', function() {
