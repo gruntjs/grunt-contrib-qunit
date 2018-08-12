gruntjs/grunt-contrib-qunit

commit b35e7ce1b45090465947a549b4c12fa37ab52b8b

feat(headless-chrome): use headless chrome instead of phantomjs (#147)
* switch to using Headless Chrome

* include chrome directory

* try to get Travis CI working

* Fix Appveyor builds by disabling the http2 experimental warning

* remove node 9 from appveyor matrix since http2 experimental warnings can't be suppressed

Gruntfile.js
@@ -10,6 +10,21 @@
 
 module.exports = function(grunt) {
 
+  // If we are running in CI, we need some puppeteer arguments
+  // to be set on every task.
+  function getGlobalOptions() {
+    if (process.env.CI) {
+      // Running in 
+      return {
+        puppeteer: {
+          args: [
+            "--no-sandbox"
+          ]
+        }
+      };
+    }
+  }
+
   // Project configuration.
   grunt.initConfig({
     jshint: {
@@ -34,6 +49,7 @@ module.exports = function(grunt) {
 
     // Unit tests.
     qunit: {
+      options: getGlobalOptions(),
       allTests: ['test/*{1,2}.html'],
       individualTests: {
         files: [{

chrome/bridge.js
@@ -6,8 +6,7 @@
  * Licensed under the MIT license.
  */
 
-/* global QUnit:true, alert:true */
-
+/* global QUnit:true */
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     require(['qunit'], factory);
@@ -20,10 +19,9 @@
   // Don't re-order tests.
   QUnit.config.reorder = false;
 
-  // Send messages to the parent PhantomJS process via alert! Good times!!
+  // Send messages to the Node process
   function sendMessage() {
-    var args = [].slice.call(arguments);
-    alert(JSON.stringify(args));
+    self.__grunt_contrib_qunit__.apply(self, [].slice.call(arguments));
   }
 
   // These methods connect QUnit to PhantomJS.

tasks/qunit.js
@@ -9,35 +9,125 @@
 'use strict';
 
 // Nodejs libs.
+var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var EventEmitter2 = require('eventemitter2');
+// NPM libs.
+var Promise = require('bluebird');
+var puppeteer = require('puppeteer');
+
+// Shared functions
+
+// Allow an error message to retain its color when split across multiple lines.
+function formatMessage (str) {
+  return String(str).split('\n')
+    .map(function(s) {
+      return s.magenta;
+    })
+    .join('\n');
+}
+
+
+function createStatus () {
+  return {
+    passed: 0,
+    failed: 0,
+    skipped: 0,
+    todo: 0,
+    runtime: 0,
+    assertions: {
+      passed: 0,
+      failed: 0
+    }
+  };
+}
+
+function mergeStatus(statusA, statusB) {
+  statusA.passed += statusB.passed;
+  statusA.failed += statusB.failed;
+  statusA.skipped += statusB.skipped;
+  statusA.todo += statusB.todo;
+  statusA.runtime += statusB.runtime;
+  statusA.assertions.passed += statusB.assertions.passed;
+  statusA.assertions.failed += statusB.assertions.failed;
+}
+
+function generateMessage(status) {
+  var totalTests = status.passed + status.failed + status.skipped + status.todo;
+  var totalAssertions = status.assertions.passed + status.assertions.failed;
+
+  return [
+    totalTests,
+    ' tests completed with ',
+    status.failed,
+    ' failed, ' +
+    status.skipped,
+    ' skipped, and ',
+    status.todo,
+    ' todo. \n' +
+    totalAssertions,
+    ' assertions (in ',
+    status.runtime,
+    'ms), passed: ' +
+    status.assertions.passed,
+    ', failed: ',
+    status.assertions.failed
+  ].join('');
+}
+
+// Copied from QUnit source code
+function generateHash (module) {
+  var hex;
+  var i = 0;
+  var hash = 0;
+  var str = module + '\x1C' + undefined;
+  var len = str.length;
+
+  for (; i < len; i++) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(i);
+    hash |= 0;
+  }
+
+  // Convert the possibly negative integer hash code into an 8 character
+  // hex string, which isn't strictly necessary but increases user understanding
+  // that the id is a SHA-like hash
+  hex = (0x100000000 + hash).toString(16);
+  if (hex.length < 8) {
+    hex = '0000000' + hex;
+  }
+
+  return hex.slice(-8);
+}
+
+function getFullUrl(url) {
+  if ((url.length < 4) || (url.substring(0,4) !== 'http')) {
+    return 'file://' + path.resolve(__dirname, '..', url);
+  } else {
+    return url;
+  }
+}
 
 module.exports = function(grunt) {
-  // External lib.
-  var phantomjs = require('grunt-lib-phantomjs').init(grunt);
+
+  var eventBus = new EventEmitter2({wildcard: true, maxListeners: 0});
 
   // Keep track of the last-started module and test. Additionally, keep track
   // of status for individual test files and the entire test suite.
   var options;
+  var puppeteerLaunchOptions;
   var currentModule;
   var currentTest;
   var currentStatus;
   var status;
+  var browser;
+  var page;
 
   // Keep track of the last-started test(s).
   var unfinished = {};
 
   // Get an asset file, local to the root of the project.
   var asset = path.join.bind(null, __dirname, '..');
-
-  // Allow an error message to retain its color when split across multiple lines.
-  function formatMessage (str) {
-    return String(str).split('\n')
-      .map(function(s) {
-        return s.magenta;
-      })
-      .join('\n');
-  }
 
   // If options.force then log an error, otherwise exit with a warning
   function warnUnlessForced (message) {
@@ -72,92 +162,22 @@ module.exports = function(grunt) {
     }
   }
 
-  function createStatus () {
-    return {
-      passed: 0,
-      failed: 0,
-      skipped: 0,
-      todo: 0,
-      runtime: 0,
-      assertions: {
-        passed: 0,
-        failed: 0
-      }
-    };
-  }
-
-  function mergeStatus(statusA, statusB) {
-    statusA.passed += statusB.passed;
-    statusA.failed += statusB.failed;
-    statusA.skipped += statusB.skipped;
-    statusA.todo += statusB.todo;
-    statusA.runtime += statusB.runtime;
-    statusA.assertions.passed += statusB.assertions.passed;
-    statusA.assertions.failed += statusB.assertions.failed;
-  }
-
-  function generateMessage(status) {
-    var totalTests = status.passed + status.failed + status.skipped + status.todo;
-    var totalAssertions = status.assertions.passed + status.assertions.failed;
-
-    return [
-      totalTests,
-      ' tests completed with ',
-      status.failed,
-      ' failed, ' +
-      status.skipped,
-      ' skipped, and ',
-      status.todo,
-      ' todo. \n' +
-      totalAssertions,
-      ' assertions (in ',
-      status.runtime,
-      'ms), passed: ' +
-      status.assertions.passed,
-      ', failed: ',
-      status.assertions.failed
-    ].join('');
-  }
-
-  // Copied from QUnit source code
-  function generateHash (module) {
-    var hex;
-    var i = 0;
-    var hash = 0;
-    var str = module + '\x1C' + undefined;
-    var len = str.length;
-
-    for (; i < len; i++) {
-      hash = ((hash << 5) - hash) + str.charCodeAt(i);
-      hash |= 0;
-    }
-
-    // Convert the possibly negative integer hash code into an 8 character
-    // hex string, which isn't strictly necessary but increases user understanding
-    // that the id is a SHA-like hash
-    hex = (0x100000000 + hash).toString(16);
-    if (hex.length < 8) {
-      hex = '0000000' + hex;
-    }
-
-    return hex.slice(-8);
-  }
 
   // QUnit hooks.
-  phantomjs.on('qunit.begin', function() {
+  eventBus.on('qunit.begin', function() {
     currentStatus = createStatus();
   });
 
-  phantomjs.on('qunit.moduleStart', function(name) {
+  eventBus.on('qunit.moduleStart', function(name) {
     unfinished[name] = true;
     currentModule = name;
   });
 
-  phantomjs.on('qunit.moduleDone', function(name) {
+  eventBus.on('qunit.moduleDone', function(name) {
     delete unfinished[name];
   });
 
-  phantomjs.on('qunit.log', function(result, actual, expected, message, source, todo) {
+  eventBus.on('qunit.log', function(result, actual, expected, message, source, todo) {
     if (!result && !todo) {
       failedAssertions.push({
         actual: actual,
@@ -169,12 +189,12 @@ module.exports = function(grunt) {
     }
   });
 
-  phantomjs.on('qunit.testStart', function(name) {
+  eventBus.on('qunit.testStart', function(name) {
     currentTest = (currentModule ? currentModule + ' - ' : '') + name;
     grunt.verbose.write(currentTest + '...');
   });
 
-  phantomjs.on('qunit.testDone', function(name, failed, passed, total, runtime, skipped, todo) {
+  eventBus.on('qunit.testDone', function(name, failed, passed, total, runtime, skipped, todo) {
     var testPassed = failed > 0 ? todo : !todo;
 
     if (skipped) {
@@ -204,8 +224,7 @@ module.exports = function(grunt) {
     }
   });
 
-  phantomjs.on('qunit.done', function(failed, passed, total, runtime) {
-    phantomjs.halt();
+  eventBus.on('qunit.done', function(failed, passed, total, runtime) {
 
     currentStatus.runtime += runtime;
     currentStatus.assertions.passed += passed;
@@ -224,55 +243,65 @@ module.exports = function(grunt) {
     mergeStatus(status, currentStatus);
   });
 
+
+
   // Re-broadcast qunit events on grunt.event.
-  phantomjs.on('qunit.*', function() {
+  eventBus.on('qunit.*', function() {
     var args = [this.event].concat(grunt.util.toArray(arguments));
     grunt.event.emit.apply(grunt.event, args);
   });
 
   // Built-in error handlers.
-  phantomjs.on('fail.load', function(url) {
-    phantomjs.halt();
+  eventBus.on('fail.load', function(url) {
     grunt.verbose.write('...');
     grunt.event.emit('qunit.fail.load', url);
-    grunt.log.error('PhantomJS unable to load \'' + url + '\' URI.');
+    grunt.log.error('Chrome unable to load \'' + url + '\' URI.');
 
     status.failed += 1;
   });
 
-  phantomjs.on('fail.timeout', function() {
-    phantomjs.halt();
+  eventBus.on('fail.timeout', function() {
     grunt.log.writeln();
     grunt.event.emit('qunit.fail.timeout');
-    grunt.log.error('PhantomJS timed out, possibly due to:\n' +
+    grunt.log.error('Chrome timed out, possibly due to:\n' +
         '- QUnit is not loaded correctly.\n- A missing QUnit start() call.\n' +
         '- Or, a misconfiguration of this task.');
 
     status.failed += 1;
   });
 
-  phantomjs.on('error.onError', function (msg, stackTrace) {
+  eventBus.on('error.onError', function (msg, stackTrace) {
     grunt.event.emit('qunit.error.onError', msg, stackTrace);
   });
 
-  grunt.registerMultiTask('qunit', 'Run QUnit unit tests in a headless PhantomJS instance.', function() {
+  grunt.registerMultiTask('qunit', 'Run QUnit unit tests in a headless Chrome instance.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     options = this.options({
-      // Default PhantomJS timeout.
+      // Default Chrome timeout.
       timeout: 5000,
-      // QUnit-PhantomJS bridge file to be injected.
-      inject: asset('phantomjs/bridge.js'),
+      // QUnit-Chrome bridge file to be injected.
+      inject: asset('chrome/bridge.js'),
       // Explicit non-file URLs to test.
       urls: [],
       force: false,
-      // Connect phantomjs console output to grunt output
+      // Connect Chrome console output to Grunt output
       console: true,
       // Do not use an HTTP base by default
       httpBase: false,
       summaryOnly: false
     });
+    puppeteerLaunchOptions = options.puppeteer || {};
 
+    // This task is asynchronous.
+    var done = this.async();
     var urls;
+    var bridgeFile = fs.readFileSync(options.inject, 'utf8');
+
+    if (!bridgeFile) {
+      grunt.fail.fatal('Could not load the specified Chrome/QUnit bridge file.');
+      done(false);
+      return;
+    }
 
     if (options.httpBase) {
       // If URLs are explicitly referenced, use them still
@@ -284,6 +313,16 @@ module.exports = function(grunt) {
     } else {
       // Combine any specified URLs with src files.
       urls = options.urls.concat(this.filesSrc);
+    }
+
+    // The final tasks to run before terminating the task
+    function finishTask(success) {
+      // Close the puppeteer browser
+      if (browser) {
+        browser.close();
+      }
+      // Finish the task
+      done(success);
     }
 
     function appendToUrls (queryParam, value) {
@@ -315,63 +354,108 @@ module.exports = function(grunt) {
       appendToUrls('seed', grunt.option('seed'));
     }
 
-    // This task is asynchronous.
-    var done = this.async();
-
     // Reset status.
     status = createStatus();
 
-    // Pass-through console.log statements.
-    if (options.console) {
-      phantomjs.on('console', console.log.bind(console));
-    }
-
-    // Process each filepath in-order.
-    grunt.util.async.forEachSeries(urls, function(url, next) {
-      grunt.verbose.subhead('Testing ' + url + ' ').or.write('Testing ' + url + ' ');
-
-      // Reset current module.
-      currentModule = null;
-
-      // Launch PhantomJS.
-      grunt.event.emit('qunit.spawn', url);
-      phantomjs.spawn(url, {
-        // Additional PhantomJS options.
-        options: options,
-        // Do stuff when done.
-        done: function(err) {
-          if (err) {
-            // If there was an error, abort the series.
-            done();
-          } else {
-            // Otherwise, process next url.
-            next();
-          }
+    // Instantiate headless browser
+    puppeteer.launch(Object.assign({
+      headless: true,
+    }, puppeteerLaunchOptions))
+      .then(function(b) {
+        browser = b;
+        return b.newPage();
+      })
+      .then(function(p) {
+        page = p;
+        // emit events published in bridge.js.
+        // This function exposure survives url navigations.
+        return page.exposeFunction('__grunt_contrib_qunit__', function() {
+          eventBus.emit.apply(eventBus, [].slice.call(arguments));
+        });
+      })
+      .then(function() {
+        // Pass through the console logs if instructed
+        if (options.console) {
+          page.on('console', function() {
+            var args = [].slice.apply(arguments);
+            var colors = {
+              'error': 'red',
+              'warning': 'yellow'
+            };
+            for (var i = 0; i < args.length; ++i) {
+              var txt = args[i].text();
+              var color = colors[args[i].type()];
+              grunt.log.writeln(color ? txt[color] : txt);  
+            }
+          });
         }
+
+        // Surface uncaught exceptions
+        page.on('pageerror', function() {
+          var args = [].slice.apply(arguments);
+          for (var i = 0; i < args.length; i++) {
+            eventBus.emit('error.onError', args[i]);
+          }
+        });
+
+        // Whenever a page is loaded with a new document, before scripts execute, inject the bridge file.
+        // Tell the client that when DOMContentLoaded fires, it needs to tell this
+        // script to inject the bridge. This should ensure that the bridge gets
+        // injected before any other DOMContentLoaded or window.load event handler.
+        page.evaluateOnNewDocument('if (window.QUnit) {\n' + bridgeFile + '\n} else {\n' + 'document.addEventListener("DOMContentLoaded", function() {\n' + bridgeFile + '\n});\n}\n');
+
+        return Promise.mapSeries(urls, function(url) {
+          // Reset current module.
+          currentModule = null;
+          grunt.event.emit('qunit.spawn', url);
+          grunt.verbose.subhead('Testing ' + url + ' ').or.write('Testing ' + url + ' ');
+
+          return Promise.all([
+            // Setup listeners for qunit.done / fail events
+            new Promise(function(resolve, reject) {
+              eventBus.once('qunit.done', function() {
+                setTimeout(resolve, 1);
+              });
+              eventBus.once('fail.*', function() {
+                setTimeout(function() {
+                  reject(url);
+                }, 1);
+              });
+            }),
+            // Navigate to the url to be tested
+            page.goto(getFullUrl(url), {
+              timeout: options.timeout
+            })
+          ]);
+        });
+      })
+      .then(function() {
+        // All tests have been run.
+        var message = generateMessage(status);
+        var success;
+
+        // Log results.
+        if (status.failed > 0) {
+          warnUnlessForced(message);
+        } else {
+          grunt.verbose.writeln();
+          grunt.log.ok(message);
+        }
+
+        if (options && options.force) {
+          success = true;
+        } else {
+          success = status.failed === 0;
+        }
+        // All done!
+        finishTask(success);
+      })
+      .catch(function(err) {
+        // If anything goes wrong, terminate the grunt task
+        grunt.log.error("There was an error with headless chrome");
+        grunt.fail.fatal(err);
+        finishTask(false);
       });
-    },
-    // All tests have been run.
-    function() {
-      var message = generateMessage(status);
-      var success;
-
-      // Log results.
-      if (status.failed > 0) {
-        warnUnlessForced(message);
-      } else {
-        grunt.verbose.writeln();
-        grunt.log.ok(message);
-      }
-
-      if (options && options.force) {
-        success = true;
-      } else {
-        success = status.failed === 0;
-      }
-
-      // All done!
-      done(success);
-    });
   });
 
 };

.travis.yml
@@ -1,12 +1,14 @@
 sudo: false
 
+addons:
+  chrome: stable
+
 language: node_js
 
 node_js:
-  - "0.10"
-  - "4"
   - "6"
-  - "7"
+  - "8"
+  - "9"
 
 matrix:
   fast_finish: true

AUTHORS
@@ -16,3 +16,4 @@ Shaker Islam
 Stephen Brandwood
 Mark Bailie (http://markbailie.co.uk)
 Sam Kirkpatrick
+Zach McElrath

appveyor.yml
@@ -5,16 +5,11 @@ version: "{build}"
 # What combinations to test
 environment:
   matrix:
-    - nodejs_version: "0.10"
-      platform: x86
-    - nodejs_version: "4"
-      platform: x64
-    - nodejs_version: "4"
-      platform: x86
     - nodejs_version: "6"
       platform: x86
-    - nodejs_version: "7"
+    - nodejs_version: "8"
       platform: x86
+      NODE_NO_HTTP2: 1
 
 install:
   - ps: Install-Product node $env:nodejs_version $env:platform

commit e77826ac708b05c77919543c16bf3dd945dac339

Fix regressions introduced in #147
Fixes #148
Fixes #149

Gruntfile.js
@@ -14,7 +14,6 @@ module.exports = function(grunt) {
   // to be set on every task.
   function getGlobalOptions() {
     if (process.env.CI) {
-      // Running in 
       return {
         puppeteer: {
           args: [

chrome/bridge.js
@@ -24,7 +24,7 @@
     self.__grunt_contrib_qunit__.apply(self, [].slice.call(arguments));
   }
 
-  // These methods connect QUnit to PhantomJS.
+  // These methods connect QUnit to Headless Chrome.
   QUnit.log(function(obj) {
     // What is this I don’t even
     if (obj.message === '[object Object], undefined:undefined') {

tasks/qunit.js
@@ -12,10 +12,12 @@
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
-var EventEmitter2 = require('eventemitter2');
+var EventEmitter = require('eventemitter2');
 // NPM libs.
-var Promise = require('bluebird');
+var pEachSeries = require('p-each-series');
 var puppeteer = require('puppeteer');
+
+var Promise = global.Promise;
 
 // Shared functions
 
@@ -100,22 +102,21 @@ function generateHash (module) {
   return hex.slice(-8);
 }
 
-function getFullUrl(url) {
-  if ((url.length < 4) || (url.substring(0,4) !== 'http')) {
-    return 'file://' + path.resolve(__dirname, '..', url);
-  } else {
+function getPath(url) {
+  if (url.substr( 0, 7 ) === 'http://' || url.substr( 0, 8 ) === 'https://') {
     return url;
   }
+
+  return 'file://' + path.resolve(process.cwd(), url);
 }
 
 module.exports = function(grunt) {
 
-  var eventBus = new EventEmitter2({wildcard: true, maxListeners: 0});
+  var eventBus = new EventEmitter({wildcard: true, maxListeners: 0});
 
   // Keep track of the last-started module and test. Additionally, keep track
   // of status for individual test files and the entire test suite.
   var options;
-  var puppeteerLaunchOptions;
   var currentModule;
   var currentTest;
   var currentStatus;
@@ -161,7 +162,6 @@ module.exports = function(grunt) {
       grunt.log.writeln();
     }
   }
-
 
   // QUnit hooks.
   eventBus.on('qunit.begin', function() {
@@ -225,7 +225,6 @@ module.exports = function(grunt) {
   });
 
   eventBus.on('qunit.done', function(failed, passed, total, runtime) {
-
     currentStatus.runtime += runtime;
     currentStatus.assertions.passed += passed;
     currentStatus.assertions.failed += failed;
@@ -242,8 +241,6 @@ module.exports = function(grunt) {
 
     mergeStatus(status, currentStatus);
   });
-
-
 
   // Re-broadcast qunit events on grunt.event.
   eventBus.on('qunit.*', function() {
@@ -290,17 +287,22 @@ module.exports = function(grunt) {
       httpBase: false,
       summaryOnly: false
     });
-    puppeteerLaunchOptions = options.puppeteer || {};
+    var puppeteerLaunchOptions = Object.assign({headless: true}, options.puppeteer);
 
     // This task is asynchronous.
     var done = this.async();
     var urls;
-    var bridgeFile = fs.readFileSync(options.inject, 'utf8');
 
-    if (!bridgeFile) {
-      grunt.fail.fatal('Could not load the specified Chrome/QUnit bridge file.');
-      done(false);
-      return;
+    // Read the content of the specified bridge files
+    var bridgeFiles = Array.isArray(options.inject) ? options.inject : [options.inject];
+    var bridgContents = [];
+
+    for (var i = 0; i < bridgeFiles.length; i++) {
+      try {
+        bridgContents.push(fs.readFileSync(bridgeFiles[i], 'utf8'));
+      } catch (err) {
+        grunt.fail.fatal('Could not load the specified Chrome/QUnit bridge file: ' + bridgeFiles[i]);
+      }
     }
 
     if (options.httpBase) {
@@ -358,9 +360,7 @@ module.exports = function(grunt) {
     status = createStatus();
 
     // Instantiate headless browser
-    puppeteer.launch(Object.assign({
-      headless: true,
-    }, puppeteerLaunchOptions))
+    puppeteer.launch(puppeteerLaunchOptions)
       .then(function(b) {
         browser = b;
         return b.newPage();
@@ -385,7 +385,7 @@ module.exports = function(grunt) {
             for (var i = 0; i < args.length; ++i) {
               var txt = args[i].text();
               var color = colors[args[i].type()];
-              grunt.log.writeln(color ? txt[color] : txt);  
+              grunt.log.writeln(color ? txt[color] : txt);
             }
           });
         }
@@ -402,9 +402,9 @@ module.exports = function(grunt) {
         // Tell the client that when DOMContentLoaded fires, it needs to tell this
         // script to inject the bridge. This should ensure that the bridge gets
         // injected before any other DOMContentLoaded or window.load event handler.
-        page.evaluateOnNewDocument('if (window.QUnit) {\n' + bridgeFile + '\n} else {\n' + 'document.addEventListener("DOMContentLoaded", function() {\n' + bridgeFile + '\n});\n}\n');
+        page.evaluateOnNewDocument('if (window.QUnit) {\n' + bridgContents.join(";") + '\n} else {\n' + 'document.addEventListener("DOMContentLoaded", function() {\n' + bridgContents.join(";") + '\n});\n}\n');
 
-        return Promise.mapSeries(urls, function(url) {
+        return pEachSeries(urls, function(url) {
           // Reset current module.
           currentModule = null;
           grunt.event.emit('qunit.spawn', url);
@@ -413,19 +413,12 @@ module.exports = function(grunt) {
           return Promise.all([
             // Setup listeners for qunit.done / fail events
             new Promise(function(resolve, reject) {
-              eventBus.once('qunit.done', function() {
-                setTimeout(resolve, 1);
-              });
-              eventBus.once('fail.*', function() {
-                setTimeout(function() {
-                  reject(url);
-                }, 1);
-              });
+              eventBus.once('qunit.done', function() { resolve(); });
+              eventBus.once('fail.*', function() { reject(url); });
             }),
+
             // Navigate to the url to be tested
-            page.goto(getFullUrl(url), {
-              timeout: options.timeout
-            })
+            page.goto(getPath(url), { timeout: options.timeout })
           ]);
         });
       })