2345965a1e9f2c50b1e2b11b4e63a499d3800e24
Timo Tijhof <krinkle@fastmail.com>
2022-10-11 19:57:38 +0100

    Update qunit_circular_object_fail.js

--- test/qunit_circular_object_fail.js
+++ test/qunit_circular_object_fail.js
@@ -3,7 +3,6 @@ circularObject.a.b = circularObject;
 
 QUnit.test('circular object test', function(assert) {
   assert.expect(2);
-  debugger;
   assert.equal(circularObject, {}, 'fail with circular actual.');
   assert.equal({}, circularObject, 'fail with circular expected.');
 });
